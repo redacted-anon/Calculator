675975ba1f56b0e454163e928649182b9133834c
Tyler Schwartz <tylerschwartz98@gmail.com>
2026-02-26 17:24:32 -0500

    Add switch case for operators selection

--- calculator.c
+++ calculator.c
@@ -18,14 +18,26 @@ bool status = true;
         printf("\nEnter an expression: ");
         scanf("%s", input);
 
-
+        // User can type quit to exit
         if (strcmp(input, "quit") == 0)
         {
             status = false;
         } else if (strcmp(input, "help") == 0)
         {
+            // Description of what the program does
+            printf("\n[DESCRIPTION]");
+            printf("\n\tThis is a simple calculator. In it's current form, the program only handles integers,");
+            printf("\n\tthough I will be adding login in the future to handle both integers and floating point numbers.");
+            
+            // Instructions on how to use the calculator
+            printf("\n\n[USAGE]");
             printf("\n\tPass your input as: <operand><operator><operand>");
             printf("\n\tFor example: 2+2\n\n");
+            printf("The following operators are available:\n\n");
+            printf("\t[+]\t Addition\n");
+            printf("\t[-]\t Subtraction\n");
+            printf("\t[*]\t Multiplication\n");
+            printf("\t[/]\t Division\n");
         } else
         {
             // Parse input
@@ -33,9 +45,33 @@ bool status = true;
 
             if (matched == 3)
             {
-                // The code below does not work. Need to use a switch on op to determine which case to use based on the value of op
-                int result = ("%d %c %d", (int)left, op, (int)right);
-                printf("\nResult: %d\n", result);
+                int result;
+
+                switch (op)
+                {
+                    case '+':
+                        result = left + right;
+                        break;
+
+                    case '-':
+                        result = left - right;
+                        break;
+
+                    case '*':
+                        result = left * right;
+                        break;
+
+                    case '/':
+                        if (right == 0)
+                        {
+                            printf("\n[Undefined] Division by zero\n");
+                        } else
+                        {
+                            result = left / right;
+                            break;
+                        }
+                }
+                printf("\n\tResult: %d\n\n", result);
             } else 
             {
                 printf("\n\t[Error] Invalid input or format: <num> <operator> <num>\n\n");
@@ -52,6 +88,6 @@ int main(void)
     printf("(type 'help' for help)\n");
     calculate();
 
+    printf("\n");
     return 0;
-
 }
